4480cd57e4c3b919f28a09ff9fec382a62b100f7
teacher <net94.teacher@gmail.com>
2020-02-10 14:01:45 +0900

    커넥션 설정및 테스트 완료

---
+++ test/erp_jdbc_study/ds/MySqlDataSourceTest.java
@@ -0,0 +1,24 @@
+package erp_jdbc_study.ds;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MySqlDataSourceTest {
+	protected Logger logger = LogManager.getLogger();
+
+	@Test
+	public void testGetConnection() {
+		try (Connection con = MySqlDataSource.getConnection();){
+			logger.debug(con);
+
+	        Assert.assertNotNull(con);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
